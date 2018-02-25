b8ec271c986b281f3d0daf6572757bb8de4734c5
Rod Johnson <johnsonroda@gmail.com>
2018-02-25 15:05:40 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/L1Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L1Application.class, args);
+	}
+}
